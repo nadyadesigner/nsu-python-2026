c42c223ed976944461a20b37e038dd9c482c9b75
NadezhdaFilatova <154691366+nadyadesigner@users.noreply.github.com>
2026-09-15 17:34:02 +0700

    Update lab_3.py

--- lab_3/lab_3.py
+++ lab_3/lab_3.py
@@ -67,6 +67,17 @@ print('Task 3 — Grade classifier')
 
 # Write your code below:
 
+score = int(input('enter your score: '))
+if 0 < score <= 100:
+    if score <= 60:
+        print("fail")
+    elif 60 < score <= 74:
+        print('C mark')
+    elif 75 <= score <= 89:
+        print('B mark')
+    elif 90 <= score <= 100:
+        print('A mark')
+
 print('='*30)
 # ============================================================
 print('Task 4 — Access decision')
@@ -159,3 +170,167 @@ print(f"{positives} values are positive", f"{negatives} values are negative", f"
 
 
 print('='*30)
+
+print('='*30)
+
+# ============================================================
+print('Task 8 — Count vowels')
+# ============================================================
+# Ask the user to enter a word or short text.
+# Count how many vowels it contains.
+
+word=input('enter a word or short text: ')
+#
+# Treat uppercase and lowercase equally.
+# Vowels: a e i o u
+#
+vowels = 0
+
+for i in word.lower():
+    if i in 'aeiou':
+        vowels += 1
+print(f"{vowels} vowels are in your word '{word}'")
+
+# Example:
+# Input: Artificial Intelligence
+# Output: 10
+#
+# Hint:
+# Iterate directly over the string.
+
+# Write your code below:
+
+print('='*30)
+# ============================================================
+print('Task 9 — Student results')
+# ============================================================
+scores = [85, 42, 67, 91, 58, 73, 100, 39]
+
+# Count:
+#   passed students: score >= 60
+#   failed students: score < 60
+#
+# Also print the average score.
+
+passeds = faileds = total = amounts= 0
+for i in scores:
+    if i >= 60:
+        passeds += 1
+    elif i < 60:
+        faileds += 1
+    total += i
+    amounts+=1
+print(f"passed students: {passeds}", f"failed students: {faileds}", f"Average score: {total/amounts:.2f}", sep='\n')
+
+# ============================================================
+print('Task 10 — Search and stop')
+# ============================================================
+names = ["Anna", "Boris", "Sasha", "Maria", "Oleg", "Dina"]
+
+# Ask the user for a name.
+# Search the list using a for loop.
+Flag = True
+name=input('enter your name: ')
+for i in names:
+    if name == i:
+        Flag = False
+        break
+if not Flag:
+    print('Found')
+else:
+    print('Not found')
+
+# If found:
+#   print "Found"
+#   stop immediately with break
+#
+# If not found:
+#   print "Not found"
+#
+# Do not use:
+#   if target in names
+#
+# Hint:
+# A Boolean variable such as found = False can help.
+
+# Write your code below:
+print('='*30)
+
+# ============================================================
+print('EXTRA Task 15 — Largest of three numbers')
+# ============================================================
+print('='*30)
+# Ask the user to enter three integers.
+#
+# Print the largest number.
+#
+# Do NOT use:
+#   max()
+#
+# Example:
+# Input:
+# 12
+# 7
+# 19
+#
+# Output:
+# Largest: 19
+#
+# Think carefully about equal values.
+
+max_number = 0
+number1, number2, number3 = int(input('enter a first number: ')), int(input('enter a second number: ')), int(input('enter a third number: '))
+
+numbers=[number1, number2, number3]
+for i in numbers:
+    if i>max_number:
+        max_number = i
+    else:
+        continue
+print(f"Maximum number is {max_number}")
+
+print('='*30)
+# Write your code below:
+#I go through each number and check - the index is greater than my current maximum. If it is, the value of the maximum variable becomes equal to the value of the index.
+
+# ============================================================
+print('EXTRA Task 16 — Number statistics')
+# ============================================================
+
+numbers = [12, -4, 7, 0, 15, -9, 8, -2, 0, 21]
+
+# Using one for loop, calculate:
+#   number of positive values
+positives = negatives = zeros = sum_positives = sum_negatives = 0
+
+for i in numbers:
+    if i > 0:
+        positives += 1
+        sum_positives += i
+    if i == 0:
+        zeros += 1
+    if i < 0:
+        negatives += 1
+        sum_negatives += i
+print(f"Positive: {positives}", f"Negatives: {negatives}", f"Zero: {zeros}",
+      f"Positive sum {sum_positives}", f"Negative sum: {sum_negatives}",sep='\n')
+#   number of negative values
+#   number of zeros
+#   sum of positive values
+#   sum of negative values
+#
+# Expected:
+# Positive: 5
+# Negative: 3
+# Zero: 2
+# Positive sum: 63
+# Negative sum: -15
+#
+# Do not manually calculate the values.
+
+# Write your code below:
+
+print('='*30)
+
+
+
